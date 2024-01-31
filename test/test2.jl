2fd2c3a21bd7e04250926c5c0f7bb5132dda22c7
Bernard Mourrain <bernard.mourrain@inria.fr>
2024-01-31 12:19:03 +0100

    update rand_joint_diag

--- test/test2.jl
+++ test/test2.jl
@@ -1,10 +1,10 @@
 using JointDiag, LinearAlgebra
 
 N = 4
-E = randn(N,N)
+E0 = randn(N,N)
 
-M1 = E*diagm([1,1,2,2])*inv(E)
-M2 = E*diagm([-1,-1,0,3])*inv(E)
+M1 = E0*diagm([1,1,2,2])*inv(E0)
+M2 = E0*diagm([-1,-1,0,3])*inv(E0)
 
 M = [M1,M2]
 
