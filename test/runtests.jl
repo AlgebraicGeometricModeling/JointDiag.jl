f774c8b613665b796800865bd1a6711edef20e3f
Benoît Legat <benoit.legat@gmail.com>
2024-02-07 11:21:47 +0100

    Add test1, test2 and test3 to runtests (#2)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,1 +1,10 @@
+using Test
+
 include("schur.jl")
+for file in readdir(joinpath(@__DIR__))
+    if startswith(file, "test") && endswith(file, ".jl")
+        @testset "$file" begin
+            include(file)
+        end
+    end
+end
